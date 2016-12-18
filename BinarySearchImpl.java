ca094f349f10266d1489485d3fbfef99e6e080c3
bern422 <bern422@gmail.com>
2016-12-18 14:41:21 -0500

    binary search

--- BinarySearchImpl.java
+++ BinarySearchImpl.java
@@ -3,22 +3,32 @@ import java.lang.*;
 
 public class BinarySearchImpl {
   public static void main(String[] args) {
-    int[] arr = {6, 10, 2, 8, 3, 9, 1, 4, 5, 7};
+    int[] arr = {1, 2, 3, 5, 5, 5, 5, 5, 6, 7, 8, 9, 10};
 
-    mergesort(arr, 0, arr.length-1);
+    int ind2 = binarysearch(arr, 2);
+    int ind4 = binarysearch(arr, 4);
+    int ind5 = binarysearch(arr, 5);
 
-    for(int i: arr) {
-      System.out.print(i + " ");
+    System.out.println("index of 2 is "+ind2);
+    System.out.println("index of 4 is "+ind4);
+    System.out.println("index of 5 is "+ind5);
+  }
+
+  public static int binarysearch(int[] arr, int k) {
+    int mid = 0;
+    int lo = 0;
+    int hi = arr.length-1;
+
+    while(lo <= hi) {
+      mid = (lo + hi)/2;
+      if (k < arr[mid]) {
+        hi = mid-1;
+      } else if (k > arr[mid]) {
+        lo = mid+1;
+      } else {
+        return mid;
+      }
     }
-    System.out.println();
-  }
-
-  public static void mergesort(int[] arr, int lo, int hi) {
-    int mid = (lo+hi)/2;
-
-  }
-
-  public static void merge(int lo, int hi) {
-
+    return -1;
   }
 }
