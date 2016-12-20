e849c0fe505acbf3748d3ef37a5501c945dcb53e
bern422 <bern422@gmail.com>
2016-12-20 12:17:15 -0500

    ZeroOneKnapsack.java

--- ZeroOneKnapsack.java
+++ ZeroOneKnapsack.java
@@ -11,8 +11,10 @@ public class ZeroOneKnapsack {
     System.out.print("Total number of items: ");
     int n = in.nextInt();
 
+    System.out.println();
+
     Item[] items = new Item[n];
-    for(int i = 0; i < item.length; i++) {
+    for(int i = 0; i < items.length; i++) {
       System.out.println("Item "+i);
       System.out.print("Weight: ");
       int w = in.nextInt();
@@ -22,17 +24,48 @@ public class ZeroOneKnapsack {
       items[i] = new Item(w, v);
     }
 
-    int[][] dp = new int[n][m];
-    for(int r = 0; r < n; r++) {
-      for(int c = 0; c < m; c++) {
+    int[][] dp = new int[n][m+1];
+    for(int r = 0; r < dp.length; r++) {
+      for(int c = 0; c < dp[r].length; c++) {
         if(c == 0) {
           dp[r][c] = 0;
           continue;
         }
-
-        // if(items[r].w < c)
+        if(r == 0) {
+          if(items[r].w > c) {
+            dp[r][c] = 0;
+          } else {
+            dp[r][c] = items[r].v;
+          }
+          continue;
+        }
+        if(items[r].w > c) {
+          dp[r][c] = dp[r-1][c];
+        } else {
+          dp[r][c] = Math.max(items[r].v+dp[r-1][c-items[r].w], dp[r-1][c]);
+        }
       }
     }
+
+    System.out.println("DP Matrix");
+    for(int r = 0; r < dp.length; r++) {
+      for(int c = 0; c < dp[r].length; c++) {
+        System.out.print(dp[r][c]+" ");
+      }
+      System.out.println();
+    }
+    System.out.println();
+    System.out.println("Used items");
+
+    int r = dp.length-1; int c = dp[r].length-1;
+    while(!(c == 0)) {
+      if(dp[r-1][c] != dp[r][c]) {
+        System.out.print(r+" ");
+        c = c-items[r].w;
+      }
+      r--;
+    }
+    System.out.println();
   }
 }
 
